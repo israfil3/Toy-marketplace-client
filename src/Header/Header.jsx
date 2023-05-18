84ee2efe301cd3ed6a78d76108cda9c72adcab38
unknown <mdisrafil61@gmail.com>
2023-05-18 21:43:34 +0600

    carsoul add

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import './header.css'
 
 const Header = () => {
     return (
-        <div>
-            <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full">
-    <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-  </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-  </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-  </div>
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>
-        </div>
-    );
+        <div className='bg-green-200'>
+              <div className="carousel total-width mx-auto">
+                    <div id="item1" className="carousel-item w-full">
+                        <img  src="https://c4.wallpaperflare.com/wallpaper/1016/903/650/render-superman-black-uniform-wallpaper-preview.jpg" className="img-height" />
+                    </div> 
+                    <div id="item2" className="carousel-item w-full">
+                        <img src="https://static.gamesmen.com.au/media/catalog/product/cache/43c1b9e48526c06c9c8010675100b71d/h/o/hot_toys_marvel_iron_man_mark_iii_2_0_16_scale_12_figure_1__1.jpg" className="img-height" />
+                    </div> 
+                    <div id="item3" className="carousel-item w-full">
+                        <img src="https://cdn.shopify.com/s/files/1/0523/0955/7435/products/fullsizeoutput_1c3_1024x1024.jpg?v=1615916893" className="img-height" />
+                    </div> 
+                    <div id="item4" className="carousel-item w-full">
+                        <img src="https://www.superherohype.com/wp-content/uploads/sites/4/2022/01/Hot-Toys-Goblin-featured.jpg?w=640" className="img-height" />
+                    </div>
+                </div> 
+                    <div className="flex justify-center w-full py-2 gap-2">
+                        <a  href="#item1" className="btn btn-xs btn-outline btn-error">1</a> 
+                        <a href="#item2" className="btn btn-xs btn-outline btn-warning">2</a> 
+                        <a href="#item3" className="btn btn-xs btn-outline btn-error">3</a> 
+                        <a href="#item4" className="btn btn-xs btn-outline btn-warning">4</a>
+                    </div>
+            </div>
+            );
 };
 
 export default Header;
