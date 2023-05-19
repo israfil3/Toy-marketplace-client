6716b049d01fac64135f64cc3da825be39a36090
unknown <mdisrafil61@gmail.com>
2023-05-20 01:02:30 +0600

    footer add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
-
+import { FaBeer, FaApple,FaPaypal,FaCcVisa,FaFacebookF,FaGooglePlusG,FaInstagram,FaArrowAltCircleUp,FaRegArrowAltCircleRight} from 'react-icons/fa'
 const Footer = () => {
     return (
-        <div>
-            <h3>footer</h3>
+        <div className='bg-stone-600 text-white p-10'>
+            <div className="grid grid-cols-3 px-20 my-10 text-center">
+                <div className="text-2xl text-left">
+                    <h1 className='mb-10'>GET UPDATES FUN <br /> STUFF YOU PROBABLY WANT <br /> TO NOW ABOUT IN YOUR INBOX</h1>
+                    <div className="flex items-center gap-3">
+                    <input className='border-2  border-t-0 border-l-0 border-r-0 rounded' type="email" name="email" placeholder='Email address'  /><FaRegArrowAltCircleRight/>
+                    </div>
+                </div>
+                <div className="mx-auto">
+                 <h1 className='text-2xl mb-5'>Menu</h1>
+                 <h2>Shop All</h2>
+                 <h2>About Us</h2>
+                 <h3>Communtiy</h3>
+                 <h4>(vibes)</h4>
+
+                </div>
+                <div className="">
+                    <h1 className='text-2xl mb-5'>Support</h1>
+                    <h3>Shipping & returns</h3>
+                    <h4>help & FEQ</h4>
+                    <h4>terms Policy</h4>
+                    <h2>Privacy policy</h2>
+                    <h3>content</h3>
+                </div>
+            </div>
+          
+            <div className="flex justify-between px-20">
+               <div className="flex gap-4 text-4xl text-yellow-500">
+                    <FaApple></FaApple>
+                    <FaPaypal></FaPaypal>
+                    <FaCcVisa></FaCcVisa>
+               </div>
+                <div className="flex gap-4 text-4xl text-yellow-500">
+                    <FaFacebookF/>
+                    <FaGooglePlusG/>
+                    <FaInstagram/>
+                </div>
+                <FaArrowAltCircleUp className='text-4xl text-yellow-500'/>
+            </div>
         </div>
     );
 };
